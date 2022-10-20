fddab00611b52f1fe0dc68e4e5df216d3b6292d1
OrYoureFired <116253036+OrYoureFired@users.noreply.github.com>
2022-10-20 17:48:19 +0100

    Add files via upload

--- coursework-final.py
+++ coursework-final.py
@@ -1,21 +1,19 @@
-"""
-TO DO LIST:
-1) Add Functionality to actually save the config file
-2) Add password verification
-3) Comment and clean up code
-4) Find more efficient ways to do things (menuLoop etc.)
-"""
 # Importing Netmiko for telnet and ssh functionality.
 import netmiko
 # Importing getpass so that the password isn't shown when it's entered into the console
 # Similar to sudo prompt in UNIX-Like OSes (Linux, MacOS, BSD etc.)
 import getpass
 
+# Initialising "device_info" as a dictionary, this will store all of the information.
 device_info = {}
-
 device_info["ip"] = input("Enter the IP address of the device you would like to connect to: ")
 device_info["user"] = input("Enter the username: ")
+# Using the aforementioned getpass for the secret and the password.
 device_info["passwd"] = getpass.getpass(prompt="Enter the password: ")
+device_info["secret"] = getpass.getpass(prompt="Enter the secret: ")
+
+# Function for telnet using netmiko, incredibly easy to implement, just provide ConnectHandler
+# with information on where it's connecting to and it'll do all the work for you.
 
 def telnet():
     session = netmiko.ConnectHandler(
@@ -23,14 +21,23 @@ def telnet():
         host=device_info["ip"],
         username=device_info["user"],
         password=device_info["passwd"],
+        secret=device_info["secret"]
     )
+
+# If the session is able to find the prompt, it prints "Successfully Connected!"
 
     if session.find_prompt() != 0:
         print("Successfully Connected!")
     else:
-        print("Unable to Connect")
+        print("Unable to Connect") 
+
+# Disconnect from the session after connecting.
 
     session.disconnect()
+
+# This is the function for connecting via SSH, it's incredibly similar to the
+# telnet one, the only difference is using (device_type="cisco_ios") rather than
+# "cisco_ios_telnet"
 
 def ssh():
     session = netmiko.ConnectHandler(
@@ -38,6 +45,7 @@ def ssh():
         host=device_info["ip"],
         username=device_info["user"],
         password=device_info["passwd"],
+        secret=device_info["secret"]        
     )
 
     if session.find_prompt() != 0:
@@ -47,56 +55,63 @@ def ssh():
 
     session.disconnect()
 
+# Most of the code here is also present in the previous two functions, however it's more involved
+# since it also has to write the config to a file.
+
 def save_config():
-    menuLoop = 0
-    while menuLoop == 0:
-        connect_type = input("\nNote: Telnet is insecure and will send your config over cleartext!!!\nHow would you like to connect? (ssh/telnet): ")
-        if connect_type == "ssh":
-            connect_type = "cisco_ios"
-            menuLoop += 1
-        elif connect_type == "telnet":
-            connect_type = "cisco_ios_telnet"
-            menuLoop += 1
-        else:
-            print("Invalid Choice")
-            menuLoop = 0
 
     session = netmiko.ConnectHandler(
-        device_type=connect_type,
+        device_type="cisco_ios",
         host=device_info["ip"],
         username=device_info["user"],
         password=device_info["passwd"],
+        secret=device_info["secret"]
     )
 
     if session.find_prompt() != 0:
         print("\nSuccessfully Connected!")
     else:
         print("Unable to Connect")
+
+    session.enable() # This is equivalent to typing "enable" in Cisco IOS, required for "show running-config" to run.
+
+# Saves the entire configuration into this "config" variable.    
+    config = session.send_command("show running-config")
 	
-	print(session.send_command('show running-show'))
-	
+# Asks for the filename from the username for the saved configuration
+    fileName = input("What would you like this configuration file to be called: ")
+    fileName += ".txt" # Adds .txt file extension, not really necessarily on Linux but if imported into Windows it's nice.
+
+# Opens the file for writing, writes the configuration into the file, and closes it.
+    file = open(fileName, "w")
+    file.write(config)
+    file.close()
+
+# Confirmation message.
+    print("Configuration successfully saved!")
+# Disconnecting from the session again.
     session.disconnect()
 
+# Menu system
 def menu():
-    menuLoop = 0
-    while menuLoop == 0:
         print("\nWhat would you like to do?")
         print("1: Telnet into the device")
         print("2: SSH into the device")
-        print("3: Copy the Configuration File from the device")
+        print("3: Copy the configuration file from the device")
         print("4: Exit")
+        
         choice = input("Choose your option: ")
+        
         if choice == "1":
             telnet()
-            menuLoop += 1
         elif choice == "2":
             ssh()
-            menuLoop += 1
         elif choice == "3":
             save_config()
-            menuLoop += 1
         elif choice == "4":
-            menuLoop += 1
+            print("Bye Bye!")
         else:
             print("Invalid Choice")
-            menuLoop = 0
+            menu() # If the choice is invalid, it just runs the menu function again, which brings the menu up again.
+
+menu()
